e4f871c7e24a79685b541051f9ed42779a870b39
xiaohai-huang <jeromehuang18@gmail.com>
2022-07-07 15:07:35 +1000

    feat(web): add game grid page

--- web/pages/game-grid.tsx
+++ web/pages/game-grid.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Link from "next/link";
 import React from "react";
 import { TextBanner } from "./components/Banner/Banner";
 
@@ -12,9 +11,17 @@ function GameGrid() {
 
       <TextBanner text="Game Grid" />
 
-      <Link href="/">
-        <button>Go Home</button>
-      </Link>
+      <div style={{ marginTop: "20px", paddingRight: "8px" }}>
+        <iframe
+          title="Game Gird"
+          src="https://nhl-game-grid.netlify.app/game-grid"
+          height={2000}
+          width="100%"
+          seamless={true}
+          style={{ border: "none" }}
+        />
+      </div>
+      <div style={{ marginBottom: "30px" }} />
     </div>
   );
 }
