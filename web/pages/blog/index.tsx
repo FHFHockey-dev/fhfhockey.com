41309d3e05dabd25fffe564510d7fae3545a1fc0
xiaohai-huang <jeromehuang18@gmail.com>
2022-07-12 18:14:11 +1000

    feat(web): fetch CMS data to display posts

--- web/pages/blog/index.tsx
+++ web/pages/blog/index.tsx
@@ -1,5 +1,8 @@
 import { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import { groq } from "next-sanity";
+import { getClient } from "lib/sanity/sanity.server";
+import { urlFor } from "lib/sanity/sanity";
 
 import { TextBanner } from "../../components/Banner/Banner";
 import BlogPost from "components/BlogPost";
@@ -16,20 +19,29 @@ export type Post = {
   imageUrl: string;
 };
 
-const getText = () => {
-  return "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis laboriosam minima rerum vel dolorum consequatur in excepturi nemo dolores. Fuga sunt nam sequi ratione itaque velit consectetur dignissimos! Doloremque, aperiam.";
-};
+const postsQuery = groq`
+  *[_type == "post"] | order(publishedAt desc) {
+    _id,
+    title,
+    "slug": slug.current,
+    mainImage,
+    publishedAt,
+    summary,
+  }
+`;
 
 export const getStaticProps: GetStaticProps = async () => {
   // TODO: fetch data from CMS
-  const posts: Post[] = ["1", "2", "3", "4"].map((slug) => ({
-    slug,
-    title: `Title for post ${slug}`,
-    summary: `I am the content, and my slug is:${slug}` + getText() + getText(),
-    createdAt: new Date().toLocaleDateString(),
-    imageUrl:
-      "https://images.unsplash.com/photo-1545471977-94cac22e71ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-  }));
+  const posts = (await getClient().fetch(postsQuery)).map(
+    // @ts-ignore
+    ({ slug, title, summary, publishedAt, mainImage }) => ({
+      slug,
+      title,
+      summary,
+      createdAt: new Date(publishedAt).toLocaleDateString(),
+      imageUrl: urlFor(mainImage).url(),
+    })
+  );
 
   return {
     props: {
