e5883d863ece9849b2bd10d30f9b4ddb95c6dcf8
xiaohai-huang <jeromehuang18@gmail.com>
2022-08-01 16:27:05 +1000

    feat(web): add PPTOI data to API

--- web/pages/api/toi.ts
+++ web/pages/api/toi.ts
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { fetchNHL } from "lib/NHL/NHL_API";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { fetchNHL } from "lib/NHL/NHL_API";
+import jsdom from "jsdom";
+const { JSDOM } = jsdom;
 
 /**
  * A date in the format of yyyy-mm-dd.
@@ -16,6 +18,8 @@ type Input = {
    * End time point in the format of yyyy-mm-dd.
    */
   EndTime: DateString;
+
+  Season: string;
   /**
    * Player Id.
    */
@@ -44,9 +48,11 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Response>
 ) {
-  const { PlayerId, StartTime, EndTime } = req.body as Input;
+  const { PlayerId, Season, StartTime, EndTime } = req.body as Input;
 
-  const toiData = await fetchNHL(`/people/${PlayerId}/stats?stats=gameLog`);
+  const toiData = await fetchNHL(
+    `/people/${PlayerId}/stats?stats=gameLog&season=${Season}`
+  );
   if (toiData.message === "Internal error occurred") {
     return res.status(404).json({
       message: "Player not found",
@@ -57,6 +63,10 @@ export default async function handler(
   const games = toiData.stats[0].splits as {
     stat: { timeOnIce: string; powerPlayTimeOnIce: string };
     date: DateString;
+    game: {
+      gamePk: number;
+    };
+    isHome: boolean;
   }[];
 
   // filter dates
@@ -76,13 +86,27 @@ export default async function handler(
   }));
 
   // Calculate PPTOI
+  const PPTOI = await Promise.all(
+    filteredGames.map(async (game) => {
+      // get game type and game number
+      // e.g. "gamePk": 2021021276,
+      const gameId = game.game.gamePk.toString().slice(4).toString();
+      const individualPPTOI = parseTime(game.stat.powerPlayTimeOnIce);
+      const teamPPTOI = parseTime(await getPPTOI(Season, gameId, game.isHome));
+
+      return {
+        date: game.date,
+        value: individualPPTOI / teamPPTOI,
+      };
+    })
+  );
 
   res.status(200).json({
     success: true,
     message: "Success!",
     data: {
       TOI,
-      PPTOI: [],
+      PPTOI,
     },
   });
 }
@@ -94,7 +118,46 @@ export default async function handler(
  */
 function parseTime(timeString: string) {
   const arr = timeString.split(":");
-  const minutes = Number.parseInt(arr[0]) + Number.parseInt(arr[1]) / 60; // converting
+  const minutes = Number.parseInt(arr[0]) + Number(arr[1]) / 60; // converting
 
   return minutes;
+}
+
+/**
+ * Retrieve the PPTOI report as html string.
+ * @param season The season
+ * @param gameId The first two digits give the type of the game, the final 4 digits identify the specific game number.
+ */
+const getReportContent = (season: string, gameId: string) => {
+  const PPTOI_REPORT_URL = `https://www.nhl.com/scores/htmlreports/${season}/GS${gameId}.HTM`;
+  return fetch(PPTOI_REPORT_URL).then((res) => res.text());
+};
+
+/**
+ * Get the team PPTOI in mm:ss format
+ * @param season
+ * @param gameId
+ * @param isHome
+ * @returns team PPTOI
+ */
+async function getPPTOI(season: string, gameId: string, isHome: boolean) {
+  const content = await getReportContent(season, gameId);
+  const { document } = new JSDOM(content).window;
+  const table = document.querySelectorAll("#PenaltySummary td");
+
+  const PPTOIs = [];
+  for (const node of table) {
+    if (node.textContent === "Power Plays (Goals-Opp./PPTime)") {
+      PPTOIs.push(
+        // @ts-ignore
+        node.parentElement.parentElement.parentElement.parentElement.parentElement.lastElementChild.textContent?.split(
+          "/"
+        )[1]
+      );
+    }
+  }
+
+  // console.log(gameId, PPTOIs);
+
+  return PPTOIs[isHome ? 1 : 0] as string;
 }
