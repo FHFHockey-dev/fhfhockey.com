d3b343f076cfd68a7d17f17d202ccbbaec194293
xiaohai-huang <jeromehuang18@gmail.com>
2022-08-26 12:17:49 +1000

    feat(web): add Select component

---
+++ web/components/Select/Select.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+import { OptionsProps } from "components/Options/Options";
+import styles from "./Select.module.scss";
+import { TimeOption } from "components/TimeOptions/TimeOptions";
+import Image from "next/image";
+import classNames from "classnames";
+
+function Select({
+  className,
+  options,
+  option,
+  onOptionChange,
+}: OptionsProps<TimeOption>) {
+  const [showOptions, setShowOptions] = useState(false);
+  const currentLabel = options.find((op) => op.value === option)?.label;
+
+  return (
+    <div className={classNames(styles.selectWrapper, className)}>
+      <div
+        className={styles.select}
+        onClick={() => setShowOptions((prev) => !prev)}
+      >
+        {currentLabel}
+        <Image
+          className={classNames(styles.arrow, {
+            [styles.up]: showOptions,
+          })}
+          alt="arrow"
+          src="/pictures/arrow.svg"
+          layout="fixed"
+          width={12}
+          height={6.7}
+        />
+      </div>
+      {showOptions && (
+        <div className={styles.options}>
+          {options.map((op) => (
+            <option
+              className={classNames({ [styles.selected]: op.value === option })}
+              key={op.value}
+              onClick={() => {
+                setShowOptions(false);
+                onOptionChange(op.value);
+                console.log(op.value);
+              }}
+            >
+              {op.label}
+            </option>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Select;
