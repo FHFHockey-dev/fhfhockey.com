3c387eefe6b76b8fe1fe54c1c9a94c4a3d736773
xiaohai-huang <jeromehuang18@gmail.com>
2022-09-16 00:19:13 +1000

    style(web): update the style of category chart

--- web/components/CategoryCoverageChart/CategoryCoverageChart.tsx
+++ web/components/CategoryCoverageChart/CategoryCoverageChart.tsx
@@ -7,6 +7,7 @@ import {
   PointElement,
   LineElement,
   Tooltip,
+  Filler,
 } from "chart.js";
 
 import usePercentileRank from "hooks/usePercentileRank";
@@ -16,29 +17,77 @@ import ChartTitle, { HightText } from "components/ChartTitle";
 import styles from "./CategoryCoverageChart.module.scss";
 import { TimeOption } from "components/TimeOptions/TimeOptions";
 
-ChartJS.register(RadialLinearScale, PointElement, LineElement, Tooltip);
+ChartJS.register(RadialLinearScale, PointElement, LineElement, Tooltip, Filler);
+
+const LABELS = [
+  "GOALS",
+  "ASSISTS",
+  "PPP",
+  "HITS",
+  "BLKS",
+  "PIM",
+  "SHOTS",
+  "+/-",
+];
+
+const LABEL_PLUGIN = {
+  id: "label_with_percentage",
+  afterDraw(chart: ChartJS) {
+    const dataset = chart.data.datasets[0].data as number[];
+
+    // @ts-ignore
+    chart.scales.r._pointLabelItems.forEach((point, i) => {
+      // stay away from the center of the circle
+      // x-distance
+      // slide-distance
+      const center = { x: chart.width / 2, y: chart.height / 2 };
+      const yDistance = point.y - center.y;
+      const slideDistance = Math.sqrt(
+        Math.pow(center.x - point.x, 2) + Math.pow(center.y - point.y, 2)
+      );
+
+      // cos x / slide = new_x / 20px
+      // sin newY / 20px = yDistance / slideDistance
+      const newY = (yDistance / slideDistance) * 10;
+
+      chart.ctx.textAlign = point.textAlign;
+      let x = 0;
+      let y = point.y + newY + 20;
+      if (point.textAlign === "left") {
+        x = point.x + 10;
+      } else if (point.textAlign === "right") {
+        x = point.x - 10;
+      } else {
+        x = point.x;
+      }
+
+      chart.ctx.font = "700 16px Roboto Condensed";
+      chart.ctx.fillStyle = "white";
+      chart.ctx.fillText(LABELS[i], x, y);
+
+      // draw percentage
+      chart.ctx.font = "400 14px Roboto Condensed";
+      chart.ctx.fillStyle = "rgba(76, 167, 221, 1)";
+      dataset[i] && chart.ctx.fillText(`${dataset[i]}%`, x, y - 16);
+    });
+  },
+};
 
 const DATA = {
-  labels: [
-    "Goals",
-    "Assists",
-    "PPP",
-    "Hits",
-    "Blocks",
-    "PIM",
-    "Shots",
-    "PlusMinus",
-  ],
+  labels: LABELS,
   datasets: [
     {
       data: [0, 0, 0, 0, 0, 0, 0, 0],
       label: "Percentile",
-      fill: false,
+      fill: true,
+      backgroundColor: "rgba(43, 168, 242, 0.65)",
       borderColor: "#07aae2",
       pointBackgroundColor: "#07aae2",
       pointBorderColor: "#fff",
       pointHoverBackgroundColor: "#fff",
       pointHoverBorderColor: "#07aae2",
+      pointRadius: 0,
+      tension: 0.1,
     },
   ],
 };
@@ -48,22 +97,36 @@ const OPTIONS = {
   maintainAspectRatio: false,
   elements: {
     line: {
-      borderWidth: 3,
+      borderWidth: 2,
+      color: "rgba(255, 255, 255, 0.25)",
     },
   },
   scales: {
     r: {
+      angleLines: {
+        color: "rgba(255, 255, 255, 0.25)",
+      },
       grid: {
         color: "white",
       },
       pointLabels: {
         color: "white",
+        callback() {
+          return " ";
+        },
+      },
+      ticks: {
+        display: false,
+        stepSize: 20,
       },
       min: 0,
       max: 100,
     },
   },
-};
+  layout: {
+    padding: 30,
+  },
+} as const;
 
 type CategoryCoverageChartProps = {
   playerId: number | undefined;
@@ -99,10 +162,13 @@ function CategoryCoverageChart({
         </>
       }
     >
-      {/* <div className={styles.chartWrapper}> */}
-      {/* @ts-ignore */}
-      <Radar ref={chartRef} data={DATA} options={OPTIONS} />
-      {/* </div> */}
+      <Radar
+        // @ts-ignore
+        ref={chartRef}
+        data={DATA}
+        options={OPTIONS}
+        plugins={[LABEL_PLUGIN]}
+      />
     </Chart>
   );
 }
