a248f123eaf0aba9dfd64bd4dd9018377fcf0af8
xiaohai-huang <jeromehuang18@gmail.com>
2022-10-04 19:58:32 +0800

    feat(web): create TeamSelect component

---
+++ web/components/TeamSelect/TeamSelect.tsx
@@ -0,0 +1,76 @@
+import React, { useRef, useState } from "react";
+import Image from "next/image";
+import classNames from "classnames";
+
+import ARROW from "public/pictures/arrow.svg";
+import { getTeamLogo } from "hooks/usePlayer";
+import styles from "./TeamSelect.module.scss";
+
+type TeamSelectProps = {
+  /**
+   * An array of team info.
+   */
+  teams: { abbreviation: string; name: string }[];
+  /**
+   * Selected team abbreviation.
+   */
+  team: string;
+
+  onTeamChange: (abbreviation: string) => void;
+};
+
+function TeamSelect({ teams = [], team, onTeamChange }: TeamSelectProps) {
+  const logosRef = useRef<HTMLDivElement>(null);
+  const [position, setPosition] = useState(0);
+
+  const onPreviousClick = () => {
+    if (logosRef.current) {
+      logosRef.current.scrollBy({ left: -60 * 3 });
+    }
+  };
+
+  const onNextClick = () => {
+    if (logosRef.current) {
+      logosRef.current.scrollBy({ left: 60 * 3 });
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <button
+        className={styles.button}
+        title="show previous"
+        onClick={onPreviousClick}
+      >
+        <Image alt="go previous" src={ARROW} width={16} height={16} />
+      </button>
+      {/* logos */}
+      <div ref={logosRef} className={styles.logos}>
+        {teams.map(({ abbreviation, name }) => (
+          <button
+            key={abbreviation}
+            title={name}
+            className={classNames(styles.logo, {
+              [styles.active]: abbreviation === team,
+            })}
+            onClick={() => onTeamChange(abbreviation)}
+          >
+            <Image
+              alt={abbreviation}
+              src={getTeamLogo(name)}
+              width={60}
+              height={40}
+              layout="fixed"
+              objectFit="contain"
+            />
+          </button>
+        ))}
+      </div>
+      <button className={styles.button} title="show next" onClick={onNextClick}>
+        <Image alt="go next" src={ARROW} width={16} height={16} />
+      </button>
+    </div>
+  );
+}
+
+export default TeamSelect;
