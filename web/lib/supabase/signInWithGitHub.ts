fdcd4ef643fd81c84b6b910b30f7b6fa1c216a25
xiaohai-huang <jeromehuang18@gmail.com>
2022-10-18 10:42:12 -0400

    fix(web): redirect to the auth page after logging in

--- web/lib/supabase/signInWithGitHub.ts
+++ web/lib/supabase/signInWithGitHub.ts
@@ -1,8 +1,11 @@
 import supabase from ".";
 
 export default async function signInWithGitHub() {
-  const data = await supabase.auth.signIn({
-    provider: "github",
-  });
+  const data = await supabase.auth.signIn(
+    {
+      provider: "github",
+    },
+    { redirectTo: window.location.href }
+  );
   return data;
 }
