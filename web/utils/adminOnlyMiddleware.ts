e07924f55096b60ed13a389dad59afaebebd861c
xiaohai-huang <jeromehuang18@gmail.com>
2024-01-14 22:06:07 -0700

    feat(web): add support for cron job

--- web/utils/adminOnlyMiddleware.ts
+++ web/utils/adminOnlyMiddleware.ts
@@ -1,6 +1,7 @@
 import { SupabaseClient } from "@supabase/supabase-js";
 import { createClientWithToken } from "lib/supabase";
 import { NextApiRequest, NextApiResponse } from "next";
+import serviceRoleClient from "lib/supabase/server";
 
 interface ApiRequest extends NextApiRequest {
   supabase: SupabaseClient<any, "public", any>;
@@ -10,27 +11,38 @@ type Handler = (req: ApiRequest, res: NextApiResponse) => Promise<any>;
 
 export default function adminOnly(handler: Handler): Handler {
   return async (req, res) => {
-    // authentication check
-    const access_token = req.headers.authorization?.split(" ")[1] ?? "";
-    const client = createClientWithToken(access_token);
-    const { error: userError } = await client.auth.getUser();
-    if (userError) {
-      return res.status(401).json({
-        message: userError.message,
-        success: false,
-      });
-    }
+    const authHeader = req.headers.authorization ?? "";
+    let client: SupabaseClient;
+    // check if this is invoked by cron
+    if (invokedByCron(authHeader)) {
+      client = serviceRoleClient;
+    } else {
+      // authentication check
+      const access_token = authHeader.split(" ")[1] ?? "";
+      client = createClientWithToken(access_token);
+      const { error: userError } = await client.auth.getUser();
+      if (userError) {
+        return res.status(401).json({
+          message: userError.message,
+          success: false,
+        });
+      }
 
-    const { data } = await client.from("users").select("role").single();
-    if (data?.role !== "admin") {
-      return res.status(403).json({
-        message: "You are not an Admin.",
-        success: false,
-      });
+      const { data } = await client.from("users").select("role").maybeSingle();
+      if (data?.role !== "admin") {
+        return res.status(403).json({
+          message: "You are not an Admin.",
+          success: false,
+        });
+      }
     }
 
     // attach supabase client to request object
     req.supabase = client;
     await handler(req, res);
   };
+}
+
+function invokedByCron(authHeader: string) {
+  return authHeader === `Bearer ${process.env.CRON_SECRET}`;
 }
