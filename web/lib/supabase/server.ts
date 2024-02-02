9725593a9814c60b3d231eca0fe00171e8d1fcd4
xiaohai-huang <jeromehuang18@gmail.com>
2024-02-02 11:44:20 +0800

    chore(web): update create auth client (server-side)

--- web/lib/supabase/server.ts
+++ web/lib/supabase/server.ts
@@ -5,10 +5,16 @@ const supabaseUrl =
   process.env.NEXT_PUBLIC_SUPABASE_URL ||
   "https://fyhftlxokyjtpndbkfse.supabase.co";
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || "";
-
+// https://supabase.com/docs/reference/javascript/auth-api?example=create-auth-client-server-side
 const supabase =
   typeof window === "undefined"
-    ? createClient<Database>(supabaseUrl, supabaseKey)
+    ? createClient<Database>(supabaseUrl, supabaseKey, {
+        auth: {
+          autoRefreshToken: false,
+          persistSession: false,
+          detectSessionInUrl: false,
+        },
+      })
     : null;
 
 export default supabase!;
