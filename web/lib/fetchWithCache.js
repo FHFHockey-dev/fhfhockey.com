e952566cb92cefe2c9b41167fb8277bf10f25252
tjsusername <timbranson515@gmail.com>
2024-02-26 21:30:53 -0500

    merging

--- web/lib/fetchWithCache.js
+++ web/lib/fetchWithCache.js
@@ -1,20 +1,29 @@
-import cacheData from "memory-cache";
+import Fetch from "lib/cors-fetch"; // Importing the custom fetch module
+import cacheData from "memory-cache"; // Importing the caching module
 
 let num = 0; // Counter for logging purposes
 
 // The main function to fetch data with cache support
 export default async function fetchWithCache(url, json = true) {
-  const value = cacheData.get(url);
+  const value = cacheData.get(url); // Try to get data from cache
   if (value) {
+    // If data is found in cache, return it
     return value;
   } else {
-    const hours = 24;
-    console.log(`num: ${num} - ${url}`);
+    // If data is not found in cache, fetch it from the network
+    const hours = 24; // Cache duration
+    console.log(`num: ${num} - ${url}`); // Logging request number and URL
     num++;
-    const res = await fetch(url);
-    const data = json ? await res.json() : await res.text();
 
-    cacheData.put(url, data, hours * 1000 * 60 * 60);
-    return data;
+    // Using the new Fetch structure
+    const response = await Fetch(url)
+      .then((res) => (json ? res.json() : res.text())) // Processing response according to the expected type (json or text)
+      .catch((error) => {
+        console.error(`Fetch error: ${error}`);
+        throw error; // Rethrow the error after logging
+      });
+
+    cacheData.put(url, response, hours * 1000 * 60 * 60); // Store the fetched data in cache
+    return response; // Return the fetched data
   }
 }
