b76c44b96aea799cd7e1e81db8ec5e23461b0683
xiaohai-huang <jeromehuang18@gmail.com>
2024-04-06 11:21:47 +0800

    feat: add custom express app

---
+++ webhooks/README.md
@@ -0,0 +1,10 @@
+# Handle long-running tasks
+
+- Handle capture line combo screenshot
+
+## Environment Variables
+
+NEXT_PUBLIC_SUPABASE_URL=
+SUPABASE_SERVICE_ROLE_KEY=
+CRON_SECRET=
+PUPPETEER_ENDPOINT=
---
+++ webhooks/app.js
@@ -0,0 +1,122 @@
+import express from "express";
+import dotenv from "dotenv";
+import puppeteer from "puppeteer-core";
+import { createClient } from "@supabase/supabase-js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3002;
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || "";
+
+const supabaseServer = createClient(supabaseUrl, supabaseKey);
+
+// Middleware to check if the request is from an admin
+function adminOnly(req, res, next) {
+  // Implement your admin check logic here
+  const auth = req.get("authorization") ?? "";
+  const token = auth.split(" ").at(-1);
+  if (token === process.env.CRON_SECRET) {
+    next();
+  } else {
+    res.json({ error: "Failed to do the operation you are not an admin." });
+  }
+}
+
+app.post("/on-new-line-combo", adminOnly, async (req, res) => {
+  const gameId = Number(req.query.gameId);
+  const teamId = Number(req.query.teamId);
+  console.log(`start to handle new line combo for ${gameId} team: ${teamId}`);
+  try {
+    await saveLinemateMatrixImages(gameId, [teamId]);
+    const resp = await sendLineComboToDiscord(gameId, teamId);
+    console.log(resp);
+    res.json({
+      message: "Successfully handled the line combo for " + gameId,
+    });
+  } catch (e) {
+    console.error(e);
+    res.json({
+      error: "Failed to handle the line combo " + e.message,
+    });
+  }
+});
+
+async function saveLinemateMatrixImages(gameId, teamIds) {
+  console.log("Start to save line combo for " + gameId);
+  let old = Date.now();
+  const browser = await puppeteer.connect({
+    browserWSEndpoint: process.env.PUPPETEER_ENDPOINT,
+  });
+
+  // Create a page
+  const page = await browser.newPage();
+
+  // Go to your site
+  await page.goto(`https://fhfhockey.com/lines/line-combo/${gameId}`);
+  await page.waitForSelector(".content");
+  console.log(`show linemate duration: ${Date.now() - old}`);
+
+  old = Date.now();
+  // Hide header
+  await page.evaluate(() => {
+    const header = document.querySelector("header");
+    if (!header) return;
+    header.style.display = "none";
+  });
+
+  console.log(`hide header duration: ${Date.now() - old}`);
+
+  old = Date.now();
+
+  console.log(`get teamIds duration: ${Date.now() - old}`);
+
+  const urls = await Promise.all(
+    teamIds.map(async (teamId) => {
+      old = Date.now();
+
+      const content = await page.$(`#linemate-matrix-${teamId} > .content`);
+      const image = await content.screenshot({
+        type: "png",
+      });
+      console.log(`take one screenshot duration: ${Date.now() - old}`);
+
+      old = Date.now();
+
+      const { data, error } = await supabaseServer.storage
+        .from("images")
+        .upload(`line-combos/${gameId}-${teamId}.png`, image, {
+          cacheControl: "604800",
+          contentType: "image/png",
+          upsert: true,
+        });
+
+      console.log(`upload one image duration: ${Date.now() - old}`);
+
+      if (error) throw error;
+      return data.path;
+    })
+  );
+
+  // Close browser.
+  await browser.close();
+  return urls;
+}
+
+function sendLineComboToDiscord(gameId, teamId) {
+  return fetch(
+    `https://fhfhockey.com/api/v1/discord/send-message/line-combo/${gameId}?teamId=${teamId}`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.CRON_SECRET}`,
+      },
+    }
+  ).then((res) => res.json());
+}
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
