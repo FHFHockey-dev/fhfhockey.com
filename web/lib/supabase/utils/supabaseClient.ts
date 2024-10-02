0255599e0dc507d4e8bd73d527115e5fbace9096
tjsusername <timbranson515@gmail.com>
2024-10-02 18:56:25 -0400

    dumb

--- web/lib/supabase/utils/supabaseClient.ts
+++ web/lib/supabase/utils/supabaseClient.ts
@@ -4,7 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 
 // Initialize Supabase client
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const supabaseKey = process.env.NEXT_SUPABASE_SERVICE_ROLE_KEY || "";
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
 
 if (!supabaseUrl || !supabaseKey) {
   console.error(
