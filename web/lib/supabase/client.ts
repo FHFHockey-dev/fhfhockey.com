f23e21eaeacd592ee209719356cacc4483e9246f
tjsusername <timbranson515@gmail.com>
2024-10-02 18:50:36 -0400

    trying service role key

--- web/lib/supabase/client.ts
+++ web/lib/supabase/client.ts
@@ -6,8 +6,8 @@ import type { Database } from "./database-generated.types";
 const supabaseUrl =
   process.env.NEXT_PUBLIC_SUPABASE_URL ||
   "https://fyhftlxokyjtpndbkfse.supabase.co";
-// const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
-const supabaseKey = process.env.NEXT_SUPABASE_SERVICE_ROLE_KEY || "";
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+//const supabaseKey = process.env.NEXT_SUPABASE_SERVICE_ROLE_KEY || "";
 
 const supabase: SupabaseClient<Database> = createClient<Database>(
   supabaseUrl,
--- web/lib/supabase/utils/supabaseClient.ts
+++ web/lib/supabase/utils/supabaseClient.ts
@@ -3,8 +3,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Initialize Supabase client
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+const supabaseKey = process.env.NEXT_SUPABASE_SERVICE_ROLE_KEY || "";
 
 if (!supabaseUrl || !supabaseKey) {
   console.error(
