8e05c13b7890840ee25efcdb4d07e5e48c8c03e1
xiaohai-huang <jeromehuang18@gmail.com>
2024-10-06 18:13:44 +0800

    fix: use service role key for server client

--- web/lib/supabase/server.ts
+++ web/lib/supabase/server.ts
@@ -6,7 +6,7 @@ import type { Database } from "./database-generated.types";
 const supabaseUrl =
   process.env.NEXT_PUBLIC_SUPABASE_URL ||
   "https://fyhftlxokyjtpndbkfse.supabase.co";
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLIC_KEY || "";
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || "";
 // https://supabase.com/docs/reference/javascript/auth-api?example=create-auth-client-server-side
 const supabase =
   typeof window === "undefined"
