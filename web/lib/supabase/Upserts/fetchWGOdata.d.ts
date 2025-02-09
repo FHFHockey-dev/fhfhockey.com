09ab4d560a30bf575f1e1c73d17ab291daa8ef57
TJ <timbranson515@gmail.com>
2025-02-09 18:06:20 -0500

    type declaration file attempt v0.6

---
+++ web/lib/supabase/Upserts/fetchWGOdata.d.ts
@@ -0,0 +1,5 @@
+// fetchWGOdata.d.ts
+
+// Declare the module (this is optional if the file is co-located)
+// If you use a relative import (as recommended), you usually don’t need to declare a module name.
+export declare function main(): Promise<void>;
