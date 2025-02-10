6ef24de4b1377fb5a0c1522472d2f0b5cd3fe57d
TJ <timbranson515@gmail.com>
2025-02-10 15:21:49 -0500

    unfucked it

--- web/lib/supabase/client.ts
+++ web/lib/supabase/client.ts
@@ -6,9 +6,7 @@ import type { Database } from "./database-generated.types";
 const supabaseUrl =
   process.env.NEXT_PUBLIC_SUPABASE_URL ||
   "https://fyhftlxokyjtpndbkfse.supabase.co";
-
-// changed from NEXT_PUBLIC_SUPABASE_ANON_KEY to NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY change back if fucked
-const supabaseKey = process.env.NEXT_SUPABASE_SERVICE_ROLE_KEY || "";
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
 
 const supabase: SupabaseClient<Database> = createClient<Database>(
   supabaseUrl,
