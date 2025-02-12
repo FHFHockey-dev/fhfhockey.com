92c7fb937b2048698dcc9d8600fd29de5023360d
TJ <timbranson515@gmail.com>
2025-02-11 23:29:01 -0500

    adding "@use "sass:color";" to SCSS files - (24 Errors)

--- web/components/TeamStandingsChart/TeamStandingsChart.tsx
+++ web/components/TeamStandingsChart/TeamStandingsChart.tsx
@@ -9,7 +9,8 @@ import { createClient } from "@supabase/supabase-js";
 // SET UP SUPABASE CLIENT
 // -------------------------------
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_SUPABASE_SERVICE_ROLE_KEY;
+const supabaseAnonKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+// TAKE OUT NEXT
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Supabase URL and Anon Key must be provided.");
 }
